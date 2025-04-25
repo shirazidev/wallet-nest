d15f0841480a3d323aee29a0e8128b47441e6c37
AmirHossein Shirazi <ashzih2@gmail.com>
2025-04-26 02:46:01 +0330

    feat: add TypeOrmModule import for UserEntity in UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { WalletEntity } from '../wallet/entities/wallet.entity';
+import { UserEntity } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
