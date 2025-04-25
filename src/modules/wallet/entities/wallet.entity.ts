cdfdcc08c307a1002ef126220bdcf3055bb3599b
AmirHossein Shirazi <ashzih2@gmail.com>
2025-04-26 02:20:38 +0330

    feat: implement WalletEntity with properties and relationships

--- src/modules/wallet/entities/wallet.entity.ts
+++ src/modules/wallet/entities/wallet.entity.ts
@@ -1,1 +1,25 @@
-export class Wallet {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { WalletType } from '../enums/wallet.enum';
+import { UserEntity } from 'src/modules/user/entities/user.entity';
+
+@Entity('wallet')
+export class WalletEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+  @Column({ type: 'enum', enum: WalletType })
+  type: string;
+  @Column()
+  invoice_number: string;
+  @Column()
+  userId: number;
+  @ManyToOne(() => UserEntity, (user) => user.transactions, {onDelete: 'CASCADE'})
+  user: UserEntity;
+}
