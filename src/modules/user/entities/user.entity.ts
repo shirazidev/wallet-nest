362243771ed3f984fdd44f294276db191274137c
AmirHossein Shirazi <ashzih2@gmail.com>
2025-04-26 02:20:29 +0330

    feat: implement UserEntity with properties and relationships

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { WalletEntity } from 'src/modules/wallet/entities/wallet.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @Column({ type: 'varchar', length: 300, nullable: false })
+  fullName: string;
+  @Column()
+  mobile: string;
+  @Column({ type: 'numeric', default: 0 })
+  balance: number;
+  @OneToMany(() => WalletEntity, wallet => wallet.user, { onDelete: 'CASCADE' })
+  transactions: WalletEntity[];
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
